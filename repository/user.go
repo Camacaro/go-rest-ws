e3bb70d37f174b07da1f8d30df87970d24bfd506
Jesus Camacaro <jesus.alejandr@gmail.com>
2022-04-26 17:48:54 -0400

    repository pattern

---
+++ repository/user.go
@@ -0,0 +1,40 @@
+package repository
+
+import (
+	"context"
+	"go-rest-ws/models"
+)
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id int64) (*models.User, error)
+}
+
+/*
+	Inversion de dependencia
+
+	Los codigos deben de esstar basardo en abstraccion y no en cosas concretas
+
+	Concreta:
+	El codigo se vuelve complicado con cada cambio que se haga en la base de datos
+	Handler - GetUserByIdPostgres ... ... ... (Toda la logica del codigo)
+	Handler - GetUserByIdMongoDB ... ... ... (Toda la logica del codigo)
+
+	Absatraccion:
+	Es mejor tener un funcion de como se va a implementar y no el
+	comportamiento interno de la base de datos
+*/
+
+var implementation UserRepository
+
+func SetRepository(repository UserRepository) {
+	implementation = repository
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id int64) (*models.User, error) {
+	return implementation.GetUserById(ctx, id)
+}
