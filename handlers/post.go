0aee5157611fdd792d999c98edd8bca9f19b1828
Jesus Camacaro <jesus.alejandr@gmail.com>
2022-04-27 18:52:49 -0400

    CRUD posts

--- handlers/post.go
+++ handlers/post.go
@@ -10,16 +10,21 @@ import (
 	"strings"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/gorilla/mux"
 	"github.com/segmentio/ksuid"
 )
 
-type InsertPostRequest struct {
+type UpsertPostRequest struct {
 	PostContent string `json:"post_content"`
 }
 
 type PostResponse struct {
 	Id          string `json:"id"`
 	PostContent string `json:"post_content"`
+}
+
+type PostUpdateResponse struct {
+	Message string `json:"message"`
 }
 
 // TODO: abstract this code (token) it's use in user.go
@@ -38,7 +43,7 @@ func InsertPostHandler(s server.Server) http.HandlerFunc {
 		}
 
 		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
-			var postRequest = InsertPostRequest{}
+			var postRequest = UpsertPostRequest{}
 			if err := json.NewDecoder(r.Body).Decode(&postRequest); err != nil {
 				log.Println(err)
 				http.Error(w, err.Error(), http.StatusBadRequest)
@@ -70,6 +75,110 @@ func InsertPostHandler(s server.Server) http.HandlerFunc {
 			json.NewEncoder(w).Encode(PostResponse{
 				Id:          post.Id,
 				PostContent: post.PostContent,
+			})
+		} else {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+	}
+}
+
+func GetPostByIdHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Extraer id de la request, /id en en el endpoint main.go
+		params := mux.Vars(r)
+		post, err := repository.GetPostById(r.Context(), params["id"])
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(post)
+	}
+}
+
+// TODO: abstract this code (token) it's use in user.go
+func UpdatePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Extraer id de la request, /id en en el endpoint main.go
+		params := mux.Vars(r)
+		tokenString := strings.TrimSpace(r.Header.Get("Authorization"))
+
+		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(s.Config().JWTSecret), nil
+		})
+
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			var postRequest = UpsertPostRequest{}
+			if err := json.NewDecoder(r.Body).Decode(&postRequest); err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			post := models.Post{
+				Id:          params["id"],
+				UserId:      claims.UserId,
+				PostContent: postRequest.PostContent,
+			}
+
+			err = repository.UpdatePost(r.Context(), &post)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(PostUpdateResponse{
+				Message: "Post updated",
+			})
+		} else {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+	}
+}
+
+// TODO: abstract this code (token) it's use in user.go
+func DeletePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Extraer id de la request, /id en en el endpoint main.go
+		params := mux.Vars(r)
+		tokenString := strings.TrimSpace(r.Header.Get("Authorization"))
+
+		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(s.Config().JWTSecret), nil
+		})
+
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			err = repository.DeletePost(r.Context(), params["id"], claims.UserId)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(PostUpdateResponse{
+				Message: "Post deleted",
 			})
 		} else {
 			http.Error(w, err.Error(), http.StatusUnauthorized)
